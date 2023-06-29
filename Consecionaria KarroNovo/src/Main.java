0899841806836152c4c4981f3f4996a815e74c70
Murilo Ramalho <74578694+murilo-ramalho@users.noreply.github.com>
2023-06-28 23:27:37 -0300

    Update Main.java

--- Consecionaria KarroNovo/src/Main.java
+++ Consecionaria KarroNovo/src/Main.java
@@ -3,55 +3,60 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        List<Veiculo> veiculos = new ArrayList<>();
+    public List<Veiculo> veiculos = new ArrayList<>();
+    public void main(String[] args) {
+        boolean c = true;
+        while (c) {
+            menu();
 
-        menu();
+            Scanner A = new Scanner(System.in);
+            int opcao = A.nextInt();
 
-        Scanner A = new Scanner(System.in);
-        int opcao = A.nextInt();
-        if (opcao == 1){
-            System.out.println("Qual tipo de veículo será cadastrado?");
-            System.out.println("1 – Carro\n" +
-                                "2 – Moto\n" +
-                                "3 – Caminhão\n" +
-                                "4 – Bicicleta\n" +
-                                "5 - Voltar ao menu inicial");
-            cadastrar();
-        } else if (opcao == 2) {
-            //editar
-        } else if (opcao == 3) {
-            //consultar só 1
-        } else if (opcao == 4){
-            //excluir
-        } else if (opcao == 5) {
-            System.out.println(veiculos);
-        } else menu();
+            if (opcao == 1) {
+                System.out.println("Qual tipo de veículo será cadastrado?");
+                System.out.println("1 – Carro\n" +
+                                    "2 – Moto\n" +
+                                    "3 – Caminhão\n" +
+                                    "4 – Bicicleta\n" +
+                                    "5 - Voltar ao menu inicial");
+                int novoVeiculo = A.nextInt();
+                cadastro(novoVeiculo);
+            } else if (opcao == 2) {
+                //editar
+            } else if (opcao == 3) {
+                //consultar só 1
+            } else if (opcao == 4) {
+                //excluir
+            } else if (opcao == 5) {
+                System.out.println();
+            } else c = false;
+        }
     }
 
     public static void menu(){
-        boolean c = true;
-        while (c) {
-            System.out.println("Bem vindo a Conseccionaria KarroNovo");
-            System.out.println("o uqe deseja fazer?");
-            System.out.println("1 – Cadastrar novo veículo\n" +
-                    "2 – Editar veículo\n" +
-                    "3 – Ver informações de um veículo específico\n" +
-                    "4 – Excluir um veículo\n" +
-                    "5 – Ver todos os veículos\n" +
-                    "6 - Sair\n");
-            cadastrar();
-        }
+        System.out.println("Bem vindo a Conseccionaria KarroNovo");
+        System.out.println("o que deseja fazer?");
+        System.out.println("1 – Cadastrar novo veículo\n" +
+                            "2 – Editar veículo\n" +
+                            "3 – Ver informações de um veículo específico\n" +
+                            "4 – Excluir um veículo\n" +
+                            "5 – Ver todos os veículos\n" +
+                            "6 - Sair\n");
     }
-    public static void cadastroVeiculo(){
+    public void automotor(){
         Scanner A = new Scanner(System.in);
         System.out.println("digite qual o combustivel");
         System.out.println("G - Veículos a gasolina\n" +
-                            "A - Veículos a álcool\n" +
-                            "F - Veículos flex\n" +
-                            "E - Veículos elétricos\n" +
-                            "D - Veículos a diesel");
+                "A - Veículos a álcool\n" +
+                "F - Veículos flex\n" +
+                "E - Veículos elétricos\n" +
+                "D - Veículos a diesel");
         String combustivel = A.next();
+
+        cadastroVeiculo();
+    }
+    public void cadastroVeiculo(){
+        Scanner A = new Scanner(System.in);
 
         System.out.println("digite a cor: ");
         String cor = A.next();
@@ -64,21 +69,23 @@ public class Main {
 
         System.out.println("digite o valor");
         float valor = A.nextInt();
+
+        int novoVeiculo = A.nextInt();
+        cadastro(novoVeiculo);
     }
 
-    private static void cadastrar() {
+    public void cadastro(int novoVeiculo){
         Scanner A = new Scanner(System.in);
-        int novoVeiculo = A.nextInt();
 
         if (novoVeiculo == 1){
-            cadastroVeiculo();
+            automotor();
             System.out.println("digite a quantidade de portas: ");
             int portas = A.nextInt();
 
             //add------------------------------------------------
 
         } else if (novoVeiculo == 2){
-            cadastroVeiculo();
+            automotor();
             System.out.println("possui partida eletrica?: Y/N");
             String escolha = A.next();
             boolean e;
@@ -91,7 +98,7 @@ public class Main {
             //add------------------------------------------
 
         } else if (novoVeiculo == 3){
-            cadastroVeiculo();
+            automotor();
             System.out.println("digite a carga máxima: ");
             int carga = A.nextInt();
 
