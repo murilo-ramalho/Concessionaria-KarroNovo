c0173a790464362e7fdd354b09d34b1a94965456
Murilo Ramalho <74578694+murilo-ramalho@users.noreply.github.com>
2023-07-21 13:23:04 -0300

    Update Caminhao.java

--- Consecionaria KarroNovo/src/Caminhao.java
+++ Consecionaria KarroNovo/src/Caminhao.java
@@ -4,4 +4,20 @@ public class Caminhao extends Automotor{
     public Caminhao(String codigo, String cor, String marca, String modelo, float valor) {
         super(codigo, cor, marca, modelo, valor);
     }
+
+    public int getCarga() {
+        return carga;
+    }
+
+    public void setCarga(int carga) {
+        this.carga = carga;
+    }
+
+    public int getExios() {
+        return exios;
+    }
+
+    public void setExios(int exios) {
+        this.exios = exios;
+    }
 }
